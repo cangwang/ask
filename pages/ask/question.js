50a8f6fa26bb9f0dfdfd66da130a20f508510604
CangWang <284699931@qq.com>
2018-01-06 21:15:32 +0800

    [2018.1.6]add ask logic

--- pages/ask/question.js
+++ pages/ask/question.js
@@ -22,6 +22,69 @@ var localData = {
       "answer2": "2",
       "answer3": "3",
       "true_ans": "2"
+    },
+    {
+      "question": "1+4=？",
+      "answer1": "5",
+      "answer2": "6",
+      "answer3": "7",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+5=？",
+      "answer1": "6",
+      "answer2": "7",
+      "answer3": "8",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+6=？",
+      "answer1": "7",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+7=？",
+      "answer1": "8",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+8=？",
+      "answer1": "9",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+9=？",
+      "answer1": "10",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+10=？",
+      "answer1": "11",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+11=？",
+      "answer1": "12",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
+    },
+    {
+      "question": "1+12=？",
+      "answer1": "13",
+      "answer2": "8",
+      "answer3": "9",
+      "true_ans": "1"
     }
   ]
 }
