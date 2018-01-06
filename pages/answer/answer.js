2f4258388548857e86a76a7562032a6db689b2db
CangWang <284699931@qq.com>
2018-01-06 14:51:26 +0800

    [2018.1.6]answer page

---
+++ pages/answer/answer.js
@@ -0,0 +1,45 @@
+//answer.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    countDownSecond: 10, 
+    status:false,
+    choice:-1,
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  naviagationToAnswer:function(){
+    wx.navigateTo({
+      url: '../answer/answer',
+    })
+  },
+  onLoad: function () {
+
+  },
+  onReady:function(){
+    var second = 10;
+    var interval = setInterval(function(){
+      console.log(second)
+      this.setData({
+        countDownSecond:second
+      })
+      second--
+      if(second <0){
+        clearInterval(interval);  
+        wx.showToast({
+          title: '倒计结束',
+          complete: function(res) {
+            second = 10;
+          },
+        })
+      }
+    }.bind(this),1000)
+  },
+  
+})
